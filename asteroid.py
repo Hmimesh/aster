856dd09df50dba6f9ff595b59baa26c77b6f0bfa
github_ratzking <ben.farjun99@gmail.com>
2024-10-04 15:12:11 +0300

    colored astreoids!

--- asteroid.py
+++ asteroid.py
@@ -1,16 +1,21 @@
 from circleshape import CircleShape
 import pygame
+import random
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
         super().__init__(x, y, radius)
         self.velocity = pygame.Vector2(0, 0)
-    
+        
+        colors = ["red", "green", "blue"]
+
+        self.color= random.choice(colors)
+
     def draw(self, screen):
         # Assuming self.position is a pygame.Vector2
         pygame.draw.circle(
             screen,
-            "white",
+            self.color,
             (int(self.position.x), int(self.position.y)),  # Correctly access position
             int(self.radius),
             2
